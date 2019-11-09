d028e5c323205cb7c2be0569227af603d1c04391
t-sin <shinichi.tanaka45@gmail.com>
2019-11-09 14:14:30 +0900

    Cheap reader

--- slis/src/main.rs
+++ slis/src/main.rs
@@ -14,6 +14,12 @@ enum Sexp {
     Cons(Arc<Mutex<Sexp>>, Arc<Mutex<Sexp>>),
 }
 
+impl Sexp {
+    fn new(sexp: Sexp) -> Arc<Mutex<Sexp>> {
+        Arc::new(Mutex::new(sexp))
+    }
+}
+
 fn cheap_print(c: Arc<Mutex<Sexp>>) {
     match &*c.lock().unwrap() {
         Sexp::Null => print!("NULL"),
@@ -37,7 +43,7 @@ fn is_whitespace(c: char) -> bool {
 }
 
 fn is_delimiter(c: char) -> bool {
-    is_whitespace(c) || c == ')'
+    is_whitespace(c) || c == ')' || c == '('
 }
 
 fn skip_whitespaces(chars: &mut Peekable<Chars>) {
@@ -67,34 +73,113 @@ fn skip_comment(chars: &mut Peekable<Chars>) {
     }
 }
 
-fn cheap_read_string(chars: &mut Peekable<Chars>) -> Sexp {
-    Sexp::Nil
+fn cheap_read_string(chars: &mut Peekable<Chars>) -> Arc<Mutex<Sexp>> {
+    chars.next();
+    let mut s = String::new();
+    loop {
+        match chars.peek() {
+            Some('"') => {
+                chars.next();
+                break;
+            },
+            None => break,
+            Some(c) => s.push(chars.next().unwrap()),
+        }
+    }
+    Sexp::new(Sexp::Str(s))
 }
 
-fn cheap_read_char(chars: &mut Peekable<Chars>) -> Sexp {
-    Sexp::Nil
+fn cheap_read_char(chars: &mut Peekable<Chars>) -> Arc<Mutex<Sexp>> {
+    chars.next();
+    if let Some('\\') = chars.peek() {
+        chars.next();
+        let mut name = String::new();
+        loop {
+            if let Some(c)  = chars.peek() {
+                if !is_delimiter(*c) {
+                    name.push(chars.next().unwrap());
+                } else {
+                    break;
+                }
+            } else {
+                break;
+            }
+        }
+        match &name[..] {
+            "space" => Sexp::new(Sexp::Char(' ')),
+            "tab" => Sexp::new(Sexp::Char('\t')),
+            "newline" => Sexp::new(Sexp::Char('\n')),
+            c => {
+                if let Some(c) = name.chars().nth(0) {
+                    Sexp::new(Sexp::Char(c))
+                } else {
+                    panic!("invalid character literal (missing char itself)")
+                }
+            }
+        }
+    } else {
+        panic!("invalid character literal");
+    }
 }
 
-fn cheap_read_int(chars: &mut Peekable<Chars>) -> Sexp {
-    Sexp::Nil
+fn cheap_read_int(chars: &mut Peekable<Chars>) -> Arc<Mutex<Sexp>> {
+    let mut s = String::new();
+    s.push(chars.next().unwrap());
+    loop {
+        if let Some(c) = chars.peek() {
+            if c.is_digit(10) {
+                s.push(chars.next().unwrap());
+            } else {
+                break;
+            }
+        } else {
+            break;
+        }
+    }
+    if let Ok(n) = s.parse::<i64>() {
+        Sexp::new(Sexp::Int(n))
+    } else {
+        panic!("cannot parse {:?} as i64", s)
+    }
 }
 
-fn cheap_read_symbol(chars: &mut Peekable<Chars>) -> Sexp {
-    Sexp::Nil
+fn cheap_read_symbol(chars: &mut Peekable<Chars>) -> Arc<Mutex<Sexp>> {
+    Sexp::new(Sexp::Nil)
 }
 
-fn cheap_read_list(chars: &mut Peekable<Chars>) -> Sexp {
-    Sexp::Nil
+fn vec_to_cons(vec: &[Arc<Mutex<Sexp>>]) -> Arc<Mutex<Sexp>> {
+    if vec.len() == 0 {
+        Sexp::new(Sexp::Nil)
+    } else {
+        Sexp::new(Sexp::Cons(vec[0].clone(), vec_to_cons(&vec[1..])))
+    }
 }
 
-fn cheap_read_1(chars: &mut Peekable<Chars>) -> Sexp {
+fn cheap_read_list(chars: &mut Peekable<Chars>) -> Arc<Mutex<Sexp>> {
+    chars.next();
+    let mut vec = Vec::new();
+    loop {
+        skip_whitespaces(chars);
+        match chars.peek() {
+            Some(')') => {
+                chars.next();
+                break;
+            },
+            None => break,
+            _ => vec.push(cheap_read(chars)),
+        }
+    }
+    vec_to_cons(&vec[..])
+}
+
+fn cheap_read(chars: &mut Peekable<Chars>) -> Arc<Mutex<Sexp>> {
     skip_whitespaces(chars);
     let c = chars.peek();
     match c {
-        None => Sexp::Nil,
+        None => Sexp::new(Sexp::Nil),
         Some(';') => {
             skip_comment(chars);
-            Sexp::Nil
+            Sexp::new(Sexp::Nil)
         },
         Some(')') => panic!("unexpected close paren"),
         Some('(') => cheap_read_list(chars),
@@ -105,9 +190,16 @@ fn cheap_read_1(chars: &mut Peekable<Chars>) -> Sexp {
     }
 }
 
-fn cheap_read(s: String) -> Vec<Arc<Mutex<Sexp>>> {
+fn cheap_read_all(s: String) -> Vec<Arc<Mutex<Sexp>>> {
     let mut chars = s.chars().peekable();
     let mut values = Vec::new();
+    loop {
+        let sexp = cheap_read(&mut chars);
+        values.push(sexp);
+        if let None = chars.peek() {
+            break;
+        }
+    }
     values
 }
 
@@ -115,7 +207,7 @@ fn cheap_read(s: String) -> Vec<Arc<Mutex<Sexp>>> {
 fn main() {
     let code = "12345 #\\1 () (1) \"hoge fuga\" (1 (2 (10 20 30) (3 4) 5)) 123".to_string();
     println!("code: {:?}", code);
-    let cons_vec = cheap_read(code);
+    let cons_vec = cheap_read_all(code);
     for cons in cons_vec {
         cheap_print(cons);
         println!("");
