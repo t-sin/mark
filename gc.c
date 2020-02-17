35ba155f96b3baf43c6a7f076b4ad89a1ee1a8f9
t-sin <shinichi.tanaka45@gmail.com>
2020-02-18 00:16:04 +0900

    WIP: Primitive objects

--- gc.c
+++ gc.c
@@ -1,107 +1,51 @@
-#include<assert.h>
-#include<stdio.h>
-#include<stdlib.h>
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-struct Heap {
+#include "obj.h"
+
+struct Header {
     unsigned char flags;
     size_t size;
-    void * ptr;
-    size_t next;
+    struct Header * next;
 };
+
+struct Heap {
+    struct Header * head;
+    size_t size;
+};
+
+#define HEADER_SIZE ((size_t) sizeof(struct Header))
+#define POINTER_SIZE ((size_t) sizeof(void *))
 
 #define HEAP_LIMIT 10000
 #define GC_ALLOC 0x1
 #define GC_MARKED 0x2
 
-static struct Heap gc_heaps[HEAP_LIMIT];
-static size_t gc_last = 0;
+struct Header * free_list;
 
-size_t find_heap(size_t pos) {
-    for (size_t p=pos; p<HEAP_LIMIT; p++) {
-        if ((gc_heaps[p].flags & GC_ALLOC)) {
-            continue;
-        } else {
-            return p;
-        }
-    }
-    printf("GC: heap exausted.\n");
-    assert(0);
-}
+static struct Header gc_heaps[HEAP_LIMIT];
 
-size_t gc_malloc(size_t size) {
-    size_t pos = find_heap(gc_last);
-    void * ptr = malloc(size);
-    assert(ptr != NULL);
+size_t gc_malloc(size_t size) {}
 
-    gc_heaps[pos].flags = GC_ALLOC;
-    gc_heaps[pos].size = size;
-    gc_heaps[pos].ptr = ptr;
-    gc_heaps[pos].next = -1;
+void gc_free(size_t ptr) {}
 
-    if (gc_last == -1) {
-        gc_last = pos;
-    } else {
-        gc_heaps[gc_last].next = pos;
-        gc_last = pos;
-    }
-
-    return pos;
-}
-
-void gc_free(size_t ptr) {
-    size_t target = 0;
-    size_t rest = gc_heaps[ptr].next;
-
-    for (; target<HEAP_LIMIT; target++) {
-        if (gc_heaps[target].next == ptr) {
-            break;
-        }
-    }
-
-    assert(target < HEAP_LIMIT);
-
-    gc_heaps[target].next = rest;
-
-    gc_heaps[ptr].flags ^= GC_ALLOC;
-    gc_heaps[ptr].next = -1;
-}
-
-void gc_init(void) {
-    for (int i=0; i<HEAP_LIMIT; i++) {
-        gc_heaps[i].flags = 0;
-        gc_heaps[i].size = 0;
-        gc_heaps[i].ptr = NULL;
-        gc_heaps[i].next = -1;
-    }
-}
+void gc_init(void) {}
 
 void gc_collect(void);
 void gc_add_root(void * start, void * end);
 
-void test_simple_malloc_and_free() {
-    assert(gc_malloc(1) == 0);
-    assert(gc_malloc(2) == 1);
-    assert(gc_malloc(3) == 2);
-    assert(gc_malloc(4) == 3);
-
-    assert(gc_heaps[3].flags == GC_ALLOC);
-    assert(gc_heaps[3].size == 4);
-    assert(gc_heaps[3].next == -1);
-
-    gc_free(3);
-
-    assert(gc_heaps[2].next == -1);
-    assert(gc_heaps[3].flags == 0);
-    assert(gc_heaps[3].next == -1);
-
-    gc_free(1);
-
-    assert(gc_heaps[0].next == 2);
-}
+void test_simple_malloc_and_free() {}
 
 void test() {
+    LisObj obj;
+    obj.tag = 0x07;
+    printf("gc_tag = %d\n", LIS_GC_TAG(obj));
+    printf("tag1 = %d\n", LIS_TAG1(obj));
+    printf("tag2 = %d\n", LIS_TAG2(obj));
+    printf("tag3 = %d\n", LIS_TAG3(obj));
+
     gc_init();
-    test_simple_malloc_and_free();
     puts("ok.");
 }
 
---
+++ obj.h
@@ -0,0 +1,43 @@
+#include<stdint.h>
+
+typedef intptr_t LisWord;
+typedef uint8_t LisByte;
+
+typedef struct lis_obj {
+  LisByte tag;
+} LisObj;
+
+/* tags
+ * 
+ * least significant bit 'g' is a GC-bit
+ * 
+ * 0000000g => pointer
+ * 0000001g => int
+ * 0000011g => char
+ * xxx0101g => special values
+ * xxx0111g => any heap-allocated data
+ * 
+ * special values:
+ * 
+ * 0000101g => nil
+ * 0010101g => t
+ *
+ * heap-allocated data
+ * 
+ * 0010111g => symbol
+ * 0100111g => cons
+ * 0110111g => function
+ * 1000111g => closure
+ * 1010111g => pakcage
+ * 1100111g => timestamp
+ * 1110111g => other classes?
+ * 
+ */
+
+#define LIS_OBJ(obj) ((LisObj)(obj))
+#define LIS_WORD(obj) ((LisWord)(obj))
+
+#define LIS_GC_TAG(obj) (LIS_OBJ(obj).tag & 0x01)
+#define LIS_TAG1(o) ((LIS_OBJ(obj).tag & (0x01 << 1)) >> 1)
+#define LIS_TAG2(o) ((LIS_OBJ(obj).tag & (0x03 << 1)) >> 1)
+#define LIS_TAG3(o) ((LIS_OBJ(obj).tag & (0x07 << 1)) >> 1)
