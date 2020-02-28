c8a65233827faecbae48ca2a32c4142f702ec596
t-sin <shinichi.tanaka45@gmail.com>
2020-02-28 18:06:49 +0900

    Arrays

--- eval.c
+++ eval.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "obj.h"
 #include "eval.h"
 
+lis_obj make_nil() {
+    lis_obj nil;
+    nil.tags = 0x05 << 1 | 0x00 << 4;
+    return nil;
+}
+
 lis_obj make_t() {
     lis_obj t;
-    t.tags = 0x05 << 1;
+    t.tags = 0x05 << 1 | 0x02 << 4;
     return t;
 }
 
-lis_obj make_nil() {
-    lis_obj nil;
-    nil.tags = 0x04 << 1;
-    return nil;
-}
 
 lis_obj make_int(lis_int n) {
     lis_obj num;
@@ -29,16 +31,39 @@ lis_obj make_char(lis_int ch) {
     return c;
 }
 
+lis_obj make_array() {
+    lis_obj arr;
+    arr.tags = 0x06 << 1;
+    arr.data.array = malloc(sizeof(lis_array));
+    arr.data.array->size = 0;
+    arr.data.array->arr = NULL;
+    return arr;
+}
+
 lis_obj * eval(lis_obj * obj) {
     if (LIS_TAG3(obj) == 0x01) {
         return obj;  // integer
     } else if (LIS_TAG3(obj) == 0x03) {
         return obj;  // char
-    } else if (LIS_TAG3(obj) & 0x04) {
-        if (LIS_TAG1(obj)) {
+    } else if (LIS_TAG3(obj) == 0x05) {
+        if (LIS_TAG_TYPE(obj) == 0x02) {
             return obj;  // t
-        } else {
+        } else if (LIS_TAG_TYPE(obj) == 0x00) {
             return obj;  // nil
+        }
+    } else if (LIS_TAG3(obj) == 0x06) {
+        // built-in types
+        switch (LIS_TAG_TYPE(obj)) {
+        case 0x00:  // array
+            return obj;
+        case 0x01:  // timestamp
+        case 0x02:  // symbol
+        case 0x03:  // cons
+        case 0x04:  // environment
+        case 0x05:  // function
+        case 0x06:  // closure
+        case 0x07:  // package
+            break;
         }
     } else {
         return obj;
@@ -50,11 +75,37 @@ void print(lis_obj * obj) {
         printf("%d", obj->data.num);
     } else if (LIS_TAG3(obj) == 0x03) {
         printf("#\\%c", obj->data.ch);
-    } else if (LIS_TAG3(obj) & 0x04) {
-        if (LIS_TAG1(obj)) {
+    } else if (LIS_TAG3(obj) == 0x05) {
+        if (LIS_TAG_TYPE(obj) == 0x02) {
             printf("t");
-        } else {
+        } else if (LIS_TAG_TYPE(obj) == 0x00) {
             printf("nil");
+        } else {
+            printf("unknown special value: %d\n", obj->tags);
         }
+    } else if (LIS_TAG3(obj) == 0x06) {
+        // built-in types
+        switch (LIS_TAG_TYPE(obj)) {
+        case 0x00:  // array
+            printf("#(");
+            for (size_t i=0; i < obj->data.array->size; i++) {
+                print(&obj->data.array->arr[i]);
+                if (i+1 < obj->data.array->size) {
+                    printf(" ");
+                }
+            }
+            printf(")");
+            break;
+        case 0x01:  // timestamp
+        case 0x02:  // symbol
+        case 0x03:  // cons
+        case 0x04:  // environment
+        case 0x05:  // function
+        case 0x06:  // closure
+        case 0x07:  // package
+            break;
+        }
+    } else {
+        printf("unknown object: %d\n", obj->tags);
     }
 }
