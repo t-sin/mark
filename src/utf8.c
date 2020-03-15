b309520afe6453301504d5076c278496dd76e098
t-sin <shinichi.tanaka45@gmail.com>
2020-03-15 14:39:42 +0900

    utf-8 encoding

--- src/utf8.c
+++ src/utf8.c
@@ -47,7 +47,6 @@ void decode_first_byte(utf8_decoding_state * state) {
 
 void decode_successor_byte(utf8_decoding_state * state) {
     lis_byte byte = state->bytes[state->n];
-    printf("(byte: %x) ", byte);
 
     if (byte >> 6 == 0b10) {
         state->cp = state->cp << 6 | byte & 0x3f;
@@ -74,31 +73,58 @@ utf8_decoding_status utf8_decode_byte(utf8_decoding_state * state, lis_byte byte
         break;
     }
 
-    printf("cp = %x ", state->cp);
     if (state->status == UTF8_DECODED) {
         *out = state->cp;
     }
     return state->status;
 }
 
-bool utf8_encode_codepoint(lis_char * cp, lis_byte * out) {
-    return false;
+int utf8_encode_codepoint(lis_char cp, lis_byte * out) {
+    if (cp < 0 || cp > 0x10FFFF) {
+        return 0;
+    }
+
+    if (cp <= 0x007F) {
+        out[0] = (lis_byte)cp;
+        return 1;
+    } else if (cp >= 0x0080 && cp < 0x07FF) {
+        out[0] = cp >> 6 | 0b11000000;
+        out[1] = cp & 0b00111111 | 0b10000000;
+        return 2;
+    } else if (cp >=0x0800 && cp < 0xFFFF) {
+        out[0] = cp >> (6 + 6) | 0b11100000;
+        out[1] = cp >> 6 & 0b00111111 | 0b10000000;
+        out[2] = cp & 0b00111111 | 0b10000000;
+        return 3;
+    } else if (cp >= 0x010000 && cp < 0x10FFFF) {
+        out[0] = cp >> (6 + 6 + 6) | 0b11110000;
+        out[1] = cp >> (6 + 6) & 0b00111111 | 0b10000000;
+        out[2] = cp >> 6 & 0b00111111 | 0b10000000;
+        out[3] = cp & 0b00111111 | 0b10000000;
+        return 4;
+    }
 }
 
 void main() {
     utf8_decoding_state * state = make_utf8_decoding_state();
 
     char s[256];
+    lis_byte c[4];
+    lis_char cp;
+
     fgets(s, 255, stdin);
     for (int i=0; i<255; i++) {
-        lis_char cp;
-
-        printf("%x ", s[i]);
 
         utf8_decoding_status status = utf8_decode_byte(state, s[i], &cp);
         if (status == UTF8_DECODED) {
-            printf("%d\n", cp);
+            printf("cp=%x ", cp);
             reset_utf8_decoding_state(state);
+
+            int len = utf8_encode_codepoint(cp, c);
+            for (int j=0; j<len; j++) {
+                printf("%c", c[j]);
+            }
+            printf("\n");
         }
 
         if (s[i] == 0) break;
--- src/utf8.h
+++ src/utf8.h
@@ -21,6 +21,6 @@ utf8_decoding_state * make_utf8_decoding_state();
 void reset_utf8_decoding_state(utf8_decoding_state * state);
 
 utf8_decoding_status utf8_decode_byte(utf8_decoding_state * state, lis_byte byte, lis_char * out);
-bool utf8_encode_codepoint(lis_char * cp, lis_byte * out);
+int utf8_encode_codepoint(lis_char cp, lis_byte * out);
 
 #endif
