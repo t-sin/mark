e0a988a04421e740aeccf2a0e39669d279882078
t-sin <shinichi.tanaka45@gmail.com>
2020-03-15 23:18:13 +0900

    Tests for byte_stream

--- tests/byte_stream.c
+++ tests/byte_stream.c
@@ -1,9 +1,209 @@
+#include <stdio.h>
 #include <assert.h>
 #include <stdbool.h>
 #include <string.h>
 
 #include "../src/byte_stream.h"
 
+void test_make_stream() {
+    _stream * stream;
+    stream = _make_stream(10);
+    assert(stream->buffer_size == 10);
+
+    stream = _make_stream(5);
+    assert(stream->buffer_size == 5);
+}
+
+void test_count_filled() {
+    _stream * stream = _make_stream(10);
+    uint8_t b;
+
+    _stream_write_byte(stream, 0);
+    _stream_write_byte(stream, 1);
+    _stream_write_byte(stream, 2);
+
+    assert(_stream_filled(stream) == 3);
+
+    _stream_read_byte(stream, &b, false);
+    _stream_read_byte(stream, &b, false);
+
+    assert(_stream_filled(stream) == 1);
+}
+
+void test_write_once() {
+    _stream * stream = _make_stream(3);
+    bool ret;
+
+    assert(stream->head == 0);
+    assert(stream->tail == 0);
+
+    ret = _stream_write_byte(stream, 42);
+
+    assert(ret == true);
+    assert(stream->head == 1);
+    assert(stream->tail == 0);
+}
+
+void test_peek_empty_stream() {
+    _stream * stream = _make_stream(3);
+    bool ret;
+    uint8_t b;
+
+    assert(stream->head == 0);
+    assert(stream->tail == 0);
+
+    ret = _stream_read_byte(stream, &b, true);
+
+    assert(ret == false);
+    assert(stream->head == 0);
+    assert(stream->tail == 0);
+}
+
+void test_read_empty_stream() {
+    _stream * stream = _make_stream(3);
+    bool ret;
+    uint8_t b;
+
+    assert(stream->head == 0);
+    assert(stream->tail == 0);
+
+    ret = _stream_read_byte(stream, &b, false);
+
+    assert(ret == false);
+    assert(stream->head == 0);
+    assert(stream->tail == 0);
+}
+
+void test_peek_once() {
+    _stream * stream = _make_stream(3);
+    bool ret;
+    uint8_t b;
+
+    ret = _stream_write_byte(stream, 42);
+
+    assert(ret == true);
+    assert(stream->head == 1);
+    assert(stream->tail == 0);
+
+    _stream_read_byte(stream, &b, true);
+
+    assert(ret == true);
+    assert(stream->head == 1);
+    assert(stream->tail == 0);
+}
+
+void test_read_once() {
+    _stream * stream = _make_stream(3);
+    bool ret;
+    uint8_t b;
+
+    ret = _stream_write_byte(stream, 42);
+
+    assert(ret == true);
+    assert(stream->head == 1);
+    assert(stream->tail == 0);
+
+    _stream_read_byte(stream, &b, false);
+
+    assert(ret == true);
+    assert(b == 42);
+    assert(stream->head == 1);
+    assert(stream->tail == 1);
+}
+
+void test_ring_wraps_pointer_of_buffer_pointer() {
+    _stream * stream = _make_stream(3);
+    bool ret;
+    uint8_t b;
+
+    _stream_write_byte(stream, 1);
+    _stream_read_byte(stream, &b, false);
+    _stream_write_byte(stream, 2);
+    _stream_read_byte(stream, &b, false);
+
+    ret = _stream_write_byte(stream, 3);
+    assert(ret == true);
+    assert(stream->head == 0);
+    assert(stream->tail == 2);
+
+    ret = _stream_read_byte(stream, &b, false);
+    assert(ret == true);
+    assert(stream->head == 0);
+    assert(stream->tail == 0);
+    assert(b == 3);
+
+    ret = _stream_write_byte(stream, 4);
+    assert(ret == true);
+    assert(stream->head == 1);
+    assert(stream->tail == 0);
+
+    ret = _stream_read_byte(stream, &b, false);
+    assert(ret == true);
+    assert(stream->head == 1);
+    assert(stream->tail == 1);
+    assert(b == 4);
+}
+
+void test_buffer_extention() {  // or the case when buffer is fulfilled
+    _stream * stream = _make_stream(5);
+    bool ret;
+
+    assert(stream->buffer_size == 5);
+
+    ret = _stream_write_byte(stream, 1);
+    assert(ret == true);
+    assert(stream->buffer_size == 5);
+
+    _stream_write_byte(stream, 2);
+    assert(ret == true);
+    assert(stream->buffer_size == 10);
+
+    _stream_write_byte(stream, 3);
+    assert(ret == true);
+    assert(stream->buffer_size == 10);
+}
+
+void test_read_after_buffer_extention() {
+    _stream * stream = _make_stream(5);
+    bool ret;
+    uint8_t b;
+
+    _stream_write_byte(stream, 1);
+    _stream_write_byte(stream, 2);
+    _stream_write_byte(stream, 3);
+    _stream_write_byte(stream, 4);
+
+    ret = _stream_read_byte(stream, &b, false);
+    assert(ret == true);
+    assert(b = 1);
+
+    ret = _stream_read_byte(stream, &b, false);
+    assert(ret == true);
+    assert(b = 2);
+
+    ret = _stream_read_byte(stream, &b, false);
+    assert(ret == true);
+    assert(b = 3);
+
+    ret = _stream_read_byte(stream, &b, false);
+    assert(ret == true);
+    assert(b = 4);
+}
+
 int main() {
+    test_make_stream();
+    test_count_filled();
+
+    test_peek_empty_stream();
+    test_read_empty_stream();
+
+    test_write_once();
+    test_peek_once();
+    test_read_once();
+
+    test_ring_wraps_pointer_of_buffer_pointer();
+    test_buffer_extention();
+    test_read_after_buffer_extention();
+
     return 0;
 }
