dff995e0461a04210a97bef4b3d89ab9a2a1a455
t-sin <shinichi.tanaka45@gmail.com>
2020-03-15 23:55:28 +0900

    Empty tests for streams

---
+++ tests/stream.c
@@ -0,0 +1,14 @@
+#include <assert.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "../src/obj.h"
+#include "../src/stream.h"
+
+void test_read_write_bytes() {
+}
+
+int main() {
+    test_read_write_bytes();
+    return 0;
+}
