9e2d51cd87693e345705161cbfc9cce368400c0e
t-sin <shinichi.tanaka45@gmail.com>
2020-03-18 04:02:10 +0900

    Read symbol

--- src/read.c
+++ src/read.c
@@ -44,11 +44,7 @@ lis_obj * read_string(lis_stream * stream) {
     lis_stream * buffer = make_lis_stream(1000, LIS_STREAM_INOUT, LIS_STREAM_TEXT);
 
     while (true) {
-        if (!stream_peek_char(stream, &ch)) {
-            return NULL;
-        }
-
-        if (ch == '"') {
+        if (!stream_peek_char(stream, &ch) || ch == '"') {
             stream_read_char(stream, &ch);
 
             lis_obj * str = make_string();
@@ -72,19 +68,53 @@ lis_obj * read_string(lis_stream * stream) {
     return NULL;
 }
 
+lis_obj * read_symbol(lis_stream * stream) {
+    lis_char ch;
+    size_t size = 0;
+    lis_stream * buffer = make_lis_stream(1000, LIS_STREAM_INOUT, LIS_STREAM_TEXT);
+
+    while (true) {
+        if (!stream_peek_char(stream, &ch) ||
+            is_whitespace(ch) ||
+            is_newline(ch)) {
+
+            lis_obj * name = make_string();
+            lis_obj * sym = make_symbol(name);
+            name->data.str->size = size;
+            name->data.str->body = (lis_char *)malloc(sizeof(lis_char) * size);
+
+            for (int i=0; i<size; i++) {
+                stream_read_char(buffer, &ch);
+                name->data.str->body[i] = ch;
+            }
+
+            return sym;
+
+        } else {
+            stream_read_char(stream, &ch);
+            stream_write_char(buffer, ch);
+            size++;
+        }
+    }
+
+    return NULL;
+}
+
 lis_obj * read(lis_stream * stream) {
     skip_whitespaces(stream);
 
+    lis_obj * obj;
     lis_char ch;
     if (!stream_peek_char(stream, &ch)) {
-        return NULL;
+        obj = NULL;
     }
 
     if (ch == '"') {
         stream_read_char(stream, &ch);
-        return read_string(stream);
+        obj = read_string(stream);
     } else {
-        stream_read_char(stream, &ch);
-        return NULL;
+        obj = read_symbol(stream);
     }
+
+    return obj;
 }
