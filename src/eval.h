59f09fab1d9f2520c20d5e72f2e466d57a734c30
t-sin <shinichi.tanaka45@gmail.com>
2020-03-18 01:53:49 +0900

    Move object constructors

--- src/eval.h
+++ src/eval.h
@@ -3,15 +3,6 @@
 
 #include "obj.h"
 
-lis_obj make_nil();
-lis_obj make_t();
-lis_obj make_int(lis_int n);
-lis_obj make_char(lis_int ch);
-lis_obj make_array();
-lis_obj make_string();
-lis_obj make_symbol(lis_obj * name);
-lis_obj make_cons();
-
 lis_obj * eval(lis_obj * obj);
 
 #endif
--- src/obj.h
+++ src/obj.h
@@ -176,4 +176,13 @@ typedef struct lis_stream {
     lis_stream_element_type element_type;
 } lis_stream;
 
+lis_obj make_nil();
+lis_obj make_t();
+lis_obj make_int(lis_int n);
+lis_obj make_char(lis_int ch);
+lis_obj make_array();
+lis_obj make_string();
+lis_obj make_symbol(lis_obj * name);
+lis_obj make_cons();
+
 #endif
