2e40ab773ed8d1d79e9f8b098fcc4e3ddc6ce22c
t-sin <shinichi.tanaka45@gmail.com>
2020-03-28 15:45:12 +0900

    Package table functions

--- src/package.h
+++ src/package.h
@@ -11,4 +11,10 @@ lis_obj * add_symbol(lis_obj * package, lis_obj * symbol);
 
 package_intern_status intern(lis_obj * package, lis_obj * name, lis_obj ** sym);
 
+
+lis_package_table * package_table_init();
+void package_table_add(lis_package * package);
+void package_table_remove(lis_package * package);
+
+
 #endif
