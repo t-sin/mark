60cac9a44f4989a25c611cb3b2c7de239b70a29b
t-sin <shinichi.tanaka45@gmail.com>
2020-03-29 17:12:12 +0900

    Display error when it's not a symbol for `function`

--- src/special_form.c
+++ src/special_form.c
@@ -1,6 +1,7 @@
 #include <uchar.h>
 
 #include "obj.h"
+#include "lstring.h"
 #include "stream.h"
 #include "print.h"
 #include "special_form.h"
@@ -12,6 +13,10 @@ lis_obj * lis_sf_quote(lis_obj * obj, lis_obj * args) {
 lis_obj * lis_sf_function(lis_obj * genv, lis_obj * args) {
     if (LIS_TAG3(args) != LIS_TAG3_BUILTIN ||
         LIS_TAG_TYPE(args) != LIS_TAG_TYPE_SYM) {
+        lis_stream * stream = genv->data.env->env.global->stream_stderr->data.stream;
+        print(stream, genv, args);
+        stream_write_string(stream, LSTR(U" is not a symbol."));
+        stream_flush(stream);
         return NULL;
     }
     if (args->data.sym->fn == NULL) {
