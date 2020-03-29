1570e8eeff16db84c6bc0b4910dc2d760b724a67
t-sin <shinichi.tanaka45@gmail.com>
2020-03-29 17:51:40 +0900

    List length!!

---
+++ src/list.c
@@ -0,0 +1,20 @@
+#include "obj.h"
+#include "lstring.h"
+#include "stream.h"
+#include "print.h"
+#include "arithmetic.h"
+
+lis_obj * list_length(lis_obj * genv, lis_obj * list) {
+    if (list == genv->data.env->env.global->symbol_nil) {
+        return _make_int(0);
+    } else if (LIS_TAG3(list) != LIS_TAG3_BUILTIN ||
+        LIS_TAG_TYPE(list) != LIS_TAG_TYPE_CONS) {
+        lis_stream * stream = genv->data.env->env.global->stream_stderr->data.stream;
+        print(stream, genv, list);
+        stream_write_string(stream, LSTR(U" is not a list."));
+        stream_flush(stream);
+        return NULL;
+    } else {
+        return int_add(genv, list_length(genv, list->data.cons->cdr), _make_int(1));
+    }
+}
---
+++ src/list.h
@@ -0,0 +1,8 @@
+#ifndef __lis_list
+#define __lis_list
+
+#include "obj.h"
+
+lis_obj * list_length(lis_obj * genv, lis_obj * list);
+
+#endif
