e03a92665e4fb1eb2c69478890991c74b6e7c888
t-sin <shinichi.tanaka45@gmail.com>
2020-03-30 12:40:02 +0900

    Implement eval_args()

--- src/eval.c
+++ src/eval.c
@@ -19,7 +19,22 @@ lis_obj * apply(lis_obj * genv, lis_obj * fn, lis_obj * args) {
 }
 
 lis_obj * eval_args(lis_obj * genv, lis_obj * args) {
-    return args;
+    if (args == LIS_GENV(genv)->symbol_nil) {
+        return LIS_GENV(genv)->symbol_nil;
+    } else if (LIS_TAG3(args) != LIS_TAG3_BUILTIN ||
+               LIS_TAG_TYPE(args) != LIS_TAG_TYPE_CONS) {
+        lis_stream * buffer = make_lis_stream(1024, LIS_STREAM_OUT, LIS_STREAM_TEXT);
+        print(genv, args, buffer);
+        stream_write_string(buffer, LSTR(U" is not a list."));
+        LIS_GENV(genv)->error = _make_error(stream_output_to_string(buffer));
+
+        return NULL;
+    } else {
+        lis_obj * cons = _make_cons();
+        LIS_CONS(cons)->car = eval(genv, LIS_CONS(args)->car);
+        LIS_CONS(cons)->cdr = eval_args(genv, LIS_CONS(args)->cdr);
+        return cons;
+    }
 }
 
 lis_obj * eval_cons(lis_obj * genv, lis_obj * cons) {
