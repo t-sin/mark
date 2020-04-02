8066f38a18e066d2b921c05874d2cf5fdf4250c1
t-sin <shinichi.tanaka45@gmail.com>
2020-04-03 00:12:39 +0900

    Memo; sequences

---
+++ src/sequence.h
@@ -0,0 +1,27 @@
+#ifndef __lis_sequence
+#define __lis_sequence
+
+// seq_copy_seq
+// seq_length
+// seq_elt
+
+// seq_subseq
+// seq_concatenate
+
+// seq_map
+// seq_reduce
+
+// seq_reverse
+
+// seq_sort
+
+// seq_find
+// seq_find_if
+// seq_position
+// seq_position_if
+// seq_remove
+// seq_remove_if
+
+// seq_remove_duplicate
+
+#endif
