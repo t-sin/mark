dfd3ad3298bebd62284513849296315a65b2c299
t-sin <shinichi.tanaka45@gmail.com>
2020-04-10 17:20:37 +0900

    not-a-list error utility

--- src/core/lerror.c
+++ src/core/lerror.c
@@ -19,6 +19,13 @@ void not_int_error(lis_obj * genv, lis_obj * obj) {
     LIS_GENV(genv)->error = _make_error(stream_output_to_string(buffer));
 }
 
+void not_list_error(lis_obj * genv, lis_obj * obj) {
+    lis_stream * buffer = make_lis_stream(1024, LIS_STREAM_INOUT, LIS_STREAM_TEXT);
+    print(genv, obj, buffer);
+    stream_write_string(buffer, LSTR(U" is not a list."));
+    LIS_GENV(genv)->error = _make_error(stream_output_to_string(buffer));
+}
+
 void not_seq_error(lis_obj * genv, lis_obj * obj) {
     lis_stream * buffer = make_lis_stream(1024, LIS_STREAM_INOUT, LIS_STREAM_TEXT);
     print(genv, obj, buffer);
--- src/core/lerror.h
+++ src/core/lerror.h
@@ -3,6 +3,7 @@
 
 void not_string_error(lis_obj * genv, lis_obj * obj);
 void not_int_error(lis_obj * genv, lis_obj * obj);
+void not_list_error(lis_obj * genv, lis_obj * obj);
 void not_seq_error(lis_obj * genv, lis_obj * obj);
 
 #endif
