6cb871d40437d59a144035d26b2af39b47e86290
t-sin <shinichi.tanaka45@gmail.com>
2020-04-15 12:02:00 +0900

    Stream error

--- src/core/lerror.c
+++ src/core/lerror.c
@@ -32,3 +32,10 @@ void not_seq_error(lis_obj * genv, lis_obj * obj) {
     stream_write_string(buffer, LSTR(U" is not a sequence."));
     LIS_GENV(genv)->error = _make_error(stream_output_to_string(buffer));
 }
+
+void not_stream_error(lis_obj * genv, lis_obj * obj) {
+    lis_stream * buffer = make_lis_stream(1024, LIS_STREAM_INOUT, LIS_STREAM_TEXT);
+    print(genv, obj, buffer);
+    stream_write_string(buffer, LSTR(U" is not a stream."));
+    LIS_GENV(genv)->error = _make_error(stream_output_to_string(buffer));
+}
--- src/core/lerror.h
+++ src/core/lerror.h
@@ -5,5 +5,6 @@ void not_string_error(lis_obj * genv, lis_obj * obj);
 void not_int_error(lis_obj * genv, lis_obj * obj);
 void not_list_error(lis_obj * genv, lis_obj * obj);
 void not_seq_error(lis_obj * genv, lis_obj * obj);
+void not_stream_error(lis_obj * genv, lis_obj * obj);
 
 #endif
