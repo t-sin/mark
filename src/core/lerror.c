25a0a7816ae9883a05dc53ee77d21934a34708af
t-sin <shinichi.tanaka45@gmail.com>
2020-04-17 11:51:21 +0900

    Add error utility

--- src/core/lerror.c
+++ src/core/lerror.c
@@ -47,6 +47,13 @@ void not_package_error(lis_obj * genv, lis_obj * obj) {
     LIS_GENV(genv)->error = _make_error(stream_output_to_string(buffer));
 }
 
+void not_symbol_error(lis_obj * genv, lis_obj * obj) {
+    lis_stream * buffer = make_lis_stream(1024, LIS_STREAM_INOUT, LIS_STREAM_TEXT);
+    print(genv, obj, buffer);
+    stream_write_string(buffer, LSTR(U" is not a symbol."));
+    LIS_GENV(genv)->error = _make_error(stream_output_to_string(buffer));
+}
+
 void not_function_error(lis_obj * genv, lis_obj * obj) {
     lis_stream * buffer = make_lis_stream(1024, LIS_STREAM_INOUT, LIS_STREAM_TEXT);
     print(genv, obj, buffer);
