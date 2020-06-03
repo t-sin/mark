5fbf990995c4179ada714faff63bca9e492b47f3
t-sin <shinichi.tanaka45@gmail.com>
2020-06-03 22:09:33 +0900

    `load` reports error while read()

--- src/core/system.c
+++ src/core/system.c
@@ -4,19 +4,32 @@
 #include "lstring.h"
 #include "lerror.h"
 #include "basic_stream.h"
+#include "stream.h"
 #include "list.h"
 #include "read.h"
 #include "eval.h"
 
 bool load(lis_obj * genv, lis_string * filename) {
     lis_stream * input = stream_open(filename, LIS_STREAM_IN, LIS_STREAM_TEXT);
-    lis_obj * obj;
+    lis_char ch;
 
     if (input == NULL) {
         return false;
     }
 
-    while ((obj = read(genv, input)) != NULL) {
+    while (stream_peek_char(input, &ch)) {
+        printf("aaaaaaaa ");
+        lis_obj * obj = read(genv, input);
+
+        if (LIS_GENV(genv)->error != NULL) {
+            lis_stream * stream_stderr = LIS_STREAM(LIS_GENV(genv)->stream_stderr);
+            stream_write_string(stream_stderr, LSTR(U"ERROR: "));
+            stream_write_string(stream_stderr, LIS_ERR(LIS_GENV(genv)->error)->message);
+            stream_write_char(stream_stderr, '\n');
+            stream_flush(stream_stderr);
+            return false;
+        }
+
         eval(genv, NULL, obj);
     }
 
