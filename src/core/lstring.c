6cb96c67dd62942383a39604d46008e8b6e050a2
t-sin <shinichi.tanaka45@gmail.com>
2020-06-08 23:30:53 +0900

    Remove `size`, it's useless

--- src/core/lstring.c
+++ src/core/lstring.c
@@ -15,16 +15,14 @@
 lis_obj * u2lstring(const char32_t * ustr, size_t ustr_size) {
     lis_obj * str = _make_string();
     lis_string * _str = (lis_string *)malloc(sizeof(lis_string));
-    size_t size = 0;
     lis_char * body = (lis_char *)malloc(sizeof(lis_char) * ustr_size);
-    
-    for (int i=0; i<ustr_size; i++) {
+
+    for (int i=0; i<ustr_size-1; i++) {
         body[i] = ustr[i];
-        size++;
     }
 
     _str->body = body;
-    _str->size = size - 1;
+    _str->size = ustr_size - 1;
     str->data.str = _str;
 
     return str;
