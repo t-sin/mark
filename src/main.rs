a0fea72def74ad04c94b93a74cad3e86a8cc9064
t-sin <shinichi.tanaka45@gmail.com>
2020-10-03 16:52:50 +0900

    WIP

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+#[derive(Debug, Clone, PartialEq)]
+pub enum Op {
+    Nop,
+}
+
+pub trait Machine {
+    fn fetch(&self) -> Op;
+    fn dump(&self);
+    fn interpret_1(&self);
+}
+
+pub trait MemoryMap {}
+
+pub struct Memory {}
+
+// type Stack = Vec<Op>;
+
+#[derive(Debug, Clone)]
+pub struct VM {}
+
 fn main() {
     println!("Hello, world!");
 }
