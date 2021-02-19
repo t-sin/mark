fcf54d1a07066da050f934a05bc24fba6ce34cc6
t-sin <shinichi.tanaka45@gmail.com>
2021-02-19 19:25:43 +0900

    Remove Sciurus. bye bye!

--- src/core/bootstrap.h
+++
@@ -1,8 +0,0 @@
-#ifndef __lis_bootstrap
-#define __lis_bootstrap
-
-#include "obj.h"
-
-lis_obj * init_global_env();
-
-#endif
--- src/peripheral/bootstrap2.h
+++
@@ -1,9 +0,0 @@
-#ifndef __lis_bootstrap_stage_2
-#define __lis_bootstrap_stage_2
-
-#include "../core/obj.h"
-
-void init_runtime(lis_obj * genv);
-
-#endif
-
